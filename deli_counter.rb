c5069d10d94c3a9cffc62e12da3da8cda20e7669
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 01:42:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    "The line is currently empty."
+  elsif katz_deli.length > 0
+    "You are #{katz_deli.length} in line."
+  end
+end
